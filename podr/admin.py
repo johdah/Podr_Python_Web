456a852440072a48ce9919291ea81df46dcd96a0
Johan Dahlberg <info@johandahlberg.com>
2013-06-20 22:47:23 +0200

    Updating the admin interfac

--- podr/admin.py
+++ podr/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from podr.models import Subscription, Episode
 
-# Register your models here.
+
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    extra = 3
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['title', 'link']}),
+        ('Date information', {'fields': ['last_updated'], 'classes': ['collapse']}),
+    ]
+    date_hierarchy = 'last_updated'
+    inlines = [EpisodeInline]
+    list_display = ('title', 'last_updated')
+    search_fields = ['title']
+
+admin.site.register(Subscription, SubscriptionAdmin)
+
+
+class EpisodeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['title', 'subscription']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    date_hierarchy = 'pub_date'
+    list_display = ('title', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['title']
+
+admin.site.register(Episode, EpisodeAdmin)
--- podr/models.py
+++ podr/models.py
@@ -28,3 +28,6 @@ class Episode(models.Model):
         return self.title
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
