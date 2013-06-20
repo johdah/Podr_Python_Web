983b1ec1ce426a0d4c6ad314f7dbea9d5cab75fa
Johan Dahlberg <info@johandahlberg.com>
2013-06-20 23:00:06 +0200

    Beginning with the routing

--- podr/views.py
+++ podr/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the podr index.")
+
+def subscription(request):
+    return HttpResponse("Hello, world. You're at the subscription index.")
+
+def subscription_add(request, link):
+    return HttpResponse("You're adding subscription %s." % link)
+
+def subscription_details(request, subscription_id):
+    return HttpResponse("You're looking at subscription %s." % subscription_id)
--- podrweb/urls.py
+++ podrweb/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'podrweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('podr.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
