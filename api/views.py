a6a5b20151397771547bb29e1b18990342d34041
Johan Dahlberg <info@johandahlberg.com>
2013-07-24 00:29:45 +0200

    + TokenAuthentication works + Adding login in browseable API

--- api/views.py
+++ api/views.py
@@ -1,12 +1,22 @@
+from django.contrib.auth.models import User
 from django.core.context_processors import request
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework import viewsets, renderers
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import link, api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from api.serializers import EpisodeSerializer, PodcastSerializer
 from podr.models import Episode, Podcast
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 @api_view(('GET',))
@@ -21,11 +31,17 @@ class EpisodeViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides 'list' and 'details'
     """
+    authentication_classes = (TokenAuthentication,SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Episode.objects.all()
     serializer_class = EpisodeSerializer
 
 
 class PodcastViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (TokenAuthentication,SessionAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     queryset = Podcast.objects.all()
     serializer_class = PodcastSerializer
 
--- podrweb/urls.py
+++ podrweb/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', include('podr.urls', namespace="podr")),
     url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
     url(r'^account/', include('account.urls', namespace="account")),
     url(r'^episode/', include('episode.urls', namespace="episode")),
     url(r'^podcast/', include('podcast.urls', namespace="podcast")),
