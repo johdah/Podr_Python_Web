6ff93cc697c74da9cde90cfc7939a06711201571
Johan Dahlberg <info@johandahlberg.com>
2013-07-24 14:46:40 +0200

    + Updating API base url to be api/v1

--- api/urls.py
+++ api/urls.py
@@ -32,13 +32,13 @@ user_podcast_detail = UserPodcastViewSet.as_view({
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = format_suffix_patterns(patterns('api.views',
-    url(r'^$', api.views.APIRootView),
-    url(r'^episodes/$', episode_list, name="episode-list"),
-    url(r'^episodes/(?P<pk>[0-9]+)/$', episode_detail, name="episode-detail"),
-    url(r'^podcasts/$', podcast_list, name="podcast-list"),
-    url(r'^podcasts/(?P<pk>[0-9]+)/$', podcast_detail, name="podcast-detail"),
-    url(r'^userepisodes/$', user_episode_list, name="user-episode-list"),
-    url(r'^userepisodes/(?P<pk>[0-9]+)/$', user_episode_detail, name="user-episode-detail"),
-    url(r'^userpodcasts/$', user_podcast_list, name="user-podcast-list"),
-    url(r'^userpodcasts/(?P<pk>[0-9]+)/$', user_podcast_detail, name="user-podcast-detail")
+    url(r'^v1/$', api.views.APIRootView),
+    url(r'^v1/episodes/$', episode_list, name="episode-list"),
+    url(r'^v1/episodes/(?P<pk>[0-9]+)/$', episode_detail, name="episode-detail"),
+    url(r'^v1/podcasts/$', podcast_list, name="podcast-list"),
+    url(r'^v1/podcasts/(?P<pk>[0-9]+)/$', podcast_detail, name="podcast-detail"),
+    url(r'^v1/userepisodes/$', user_episode_list, name="user-episode-list"),
+    url(r'^v1/userepisodes/(?P<pk>[0-9]+)/$', user_episode_detail, name="user-episode-detail"),
+    url(r'^v1/userpodcasts/$', user_podcast_list, name="user-podcast-list"),
+    url(r'^v1/userpodcasts/(?P<pk>[0-9]+)/$', user_podcast_detail, name="user-podcast-detail")
 ))
