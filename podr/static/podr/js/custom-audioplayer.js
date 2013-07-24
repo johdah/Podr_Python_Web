b4a67892d50571c0e491b7cf1bcbfd00ed2ce19e
Johan Dahlberg <info@johandahlberg.com>
2013-07-24 02:07:10 +0200

    + Basic Custom Player + Load Episode works

--- podr/static/podr/js/custom-audioplayer.js
+++ podr/static/podr/js/custom-audioplayer.js
@@ -5,10 +5,87 @@ const PODR_ENCLOSURE_TIME = "podr.current.enclosure.time";
 const PODR_ENCLOSURE_TYPE = "podr.current.enclosure.type";
 const PODR_ENCLOSURE_URL = "podr.current.enclosure.url";
 
+var playerAudioElement = new Audio();
+
+jQuery(document).ready(function(){
+    container = $('#player-controls');
+    cover = $('#player-controls .cover');
+    play = $('#player-controls #play');
+    pause = $('#player-controls #pause');
+    mute = $('#player-controls #mute');
+    muted = $('#player-controls #muted');
+    close = $('#player-controls #close');
+    playerAudioElement = new Audio();
+
+    playerAudioElement.type= localStorage[PODR_ENCLOSURE_TYPE];
+    playerAudioElement.src= localStorage[PODR_ENCLOSURE_URL];
+    duration = playerAudioElement.duration;
+
+    // TODO: Turn autoplay off
+    playerAudioElement.play();
+
+
+    play.live('click', function(e) {
+        e.preventDefault();
+        playerAudioElement.play();
+
+        $(this).replaceWith('<a class="button gradient" id="pause" href="" title=""></a>');
+        container.addClass('containerLarge');
+        cover.addClass('coverLarge');
+        $('#close').fadeIn(300);
+        $('#seek').attr('max',playerAudioElement.duration);
+    });
+
+    pause.live('click', function(e) {
+        e.preventDefault();
+        playerAudioElement.pause();
+        $(this).replaceWith('<a class="button gradient" id="play" href="" title=""></a>');
+
+    });
+
+    mute.live('click', function(e) {
+        e.preventDefault();
+        playerAudioElement.volume = 0;
+        $(this).replaceWith('<a class="button gradient" id="muted" href="" title=""></a>');
+
+    });
+
+    muted.live('click', function(e) {
+        e.preventDefault();
+        playerAudioElement.volume = 1;
+        $(this).replaceWith('<a class="button gradient" id="mute" href="" title=""></a>');
+
+    });
+
+    $('#close').click(function(e) {
+        e.preventDefault();
+        container.removeClass('containerLarge');
+        cover.removeClass('coverLarge');
+        playerAudioElement.pause();
+        playerAudioElement.currentTime = 0;
+        $('#pause').replaceWith('<a class="button gradient" id="play" href="" title=""></a>');
+        $('#close').fadeOut(300);
+    });
+
+
+
+    $("#seek").bind("change", function() {
+        playerAudioElement.currentTime = $(this).val();
+        $("#seek").attr("max", playerAudioElement.duration);
+    });
+
+    playerAudioElement.addEventListener('timeupdate',function (){
+        curtime = parseInt(playerAudioElement.currentTime, 10);
+        $("#seek").attr("value", curtime);
+    });
+});
+
+
+
 // TODO: Fix save current episode to localStorage
 // TODO: Fix save current time to localStorage
 // TODO: Support for more fileTypes than mp3
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#audio-player-container").jPlayer({
         ready: function () {
             loadPlayerState();
@@ -33,21 +110,20 @@ $(document).ready(function(){
             saveCurrentState(event);
         }
     });
-});
+});*/
 
 // TODO: Does this work?
-// TODO: Support more filetypes
 function loadEpisode() {
     episodeId = $('#episode_id').text();
     episodeTitle = $('#episode_title').text();
     enclosureUrl = $('#enclosure_url').text();
     enclosureType = $('#enclosure_type').text();
 
-    $("#audio-player-container").jPlayer("setMedia", {
-        mp3: enclosureUrl
-    });
+    playerAudioElement.src = enclosureUrl;
+    playerAudioElement.type = enclosureType;
+    playerAudioElement.play();
 
-    $("#playing_episode_title").text(episodeTitle)
+    //$("#playing_episode_title").text(episodeTitle);
 
     if (!Modernizr.localstorage) { return false; }
     localStorage[PODR_EPISODE_ID] = episodeId;
@@ -62,7 +138,7 @@ function loadEpisode() {
 // TODO: Fix load current time to localStorage
 // TODO: Support for more fileTypes than mp3
 // TODO: Need support for Modernizer
-function loadPlayerState() {
+/*function loadPlayerState() {
     if (!Modernizr.localstorage) { return false; }
     if( localStorage[PODR_ENCLOSURE_URL]) {
         $("#audio-player-container").jPlayer("setMedia", {
@@ -92,4 +168,4 @@ function saveCurrentState(event) {
         console.log("Saving enclosure time: " + localStorage[PODR_ENCLOSURE_TIME]);
         $("#audio-controls-seekbar .bar").css("width", event.jPlayer.status.currentPercentAbsolute + "%");
     }
-}
+}*/
