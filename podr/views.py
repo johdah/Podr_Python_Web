866f5dee7dc211900c97aa9f31f3023c870c4749
Johan Dahlberg <info@johandahlberg.com>
2013-07-25 03:07:20 +0200

    + Adding a changelog view

--- podr/views.py
+++ podr/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {'title': 'Welcome to PodR'}
     return render(request, 'podr/index.html', context)
+
+
+def about_changelog(request):
+    context = {}
+    return render(request, 'podr/about_changelog.html', context)
--- podrweb/urls.py
+++ podrweb/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+import podr
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('podr.urls', namespace="podr")),
+    # ex: /about/
+    url(r'^about/$', podr.views.about_changelog, name='about'),
+    # ex: /about/changelog/
+    url(r'^about/changelog/$', podr.views.about_changelog, name='about_changelog'),
+    # Other
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^account/', include('account.urls', namespace="account")),
